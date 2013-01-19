182400d9bf8a9ce9dc58ee775843b47b91e3a819
corranmusk <corran.musk@googlemail.com>
2013-01-19 17:37:08 +0000

    Continued attempt at windows interface.

--- JulianDayCalculator.java
+++ JulianDayCalculator.java
@@ -22,42 +22,46 @@
  */
 
 import javax.swing.*;
-import java.awt.Event;
-import java.awt.AWTEvent;
-import java.awt.event.WindowEvent;
+import java.awt.*;
+import java.awt.event.*;
 
-import java.awt.Toolkit;
-import java.awt.Dimension;
+public class JulianDayCalculator extends JFrame implements ActionListener {
 
-public class JulianDayCalculator extends JFrame {
+    JulianDay test = new JulianDay();
+    
+    JTextField  JDay = new JTextField(20);
+    JLabel      JDayLab = new JLabel ("Julian Day");
+    JButton     JBut = new JButton("Calculate");
+    JLabel      GDate = new JLabel("GDATE:");
+    
+    public JulianDayCalculator() {
 
-	JulianDay test = new JulianDay();
-	static JulianDayCalculator window;
-	
-	public JulianDayCalculator() {
-		
-		setTitle("Julian Day Calculator");
-		//setDefaultCloseOperation(EXIT_ON_CLOSE);
-		enableEvents(AWTEvent.WINDOW_EVENT_MASK);
-	}
-	
-	protected void processWindowEvent(WindowEvent e){
-		if (e.getID() == WindowEvent.WINDOW_CLOSING) {
-			dispose();
-			System.exit(0);
-		}
-		super.processWindowEvent(e);
-	}
-	
+        super("Julian Day Calculator");
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	//enableEvents(AWTEvent.WINDOW_EVENT_MASK);
+        setSize(350,100);
+        
+        FlowLayout flow = new FlowLayout();
+        Container content = this.getContentPane();
+        content.setLayout(flow);
+        
+        content.add(JDayLab);
+        content.add(JDay);
+        content.add(JBut);
+        content.add(GDate);
+        
+        setVisible(true);
+        
+    }
+    
     public static void main(String[] args) {
-		window = new JulianDayCalculator();
+        
+        JulianDayCalculator frame = new JulianDayCalculator();
 	
-		Toolkit theKit = window.getToolkit();
-		Dimension wndSize = theKit.getScreenSize();
-		
-		window.setBounds(wndSize.width/4, wndSize.height/4,  // Position
-						 250, 400); // Size
-						 
-		window.setVisible(true);
-    }	
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
